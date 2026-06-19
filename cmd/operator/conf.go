aeb8fb6df529f9d2ca2865f8e3604dd8f83013a4
David Desmarais-Michaud <desmarais.michaud.david@gmail.com>
2026-06-19 17:51:34 -0400

    feat(PL-6131): configure operator concurrency through envvar

--- cmd/operator/conf.go
+++ cmd/operator/conf.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"os"
+	"runtime"
 
 	"github.com/davidmdm/conf"
 
@@ -13,6 +14,7 @@ type Config struct {
 	ChartCacheDir   string
 	EnvDestinations map[string]argocd.ApplicationDestination
 	HelmLogin       HelmLogin
+	Concurrency     int
 }
 
 type HelmLogin struct {
@@ -21,6 +23,8 @@ type HelmLogin struct {
 	Credentials     []byte
 }
 
+var defaultConcurrency = max(4, runtime.GOMAXPROCS(-1))
+
 func GetConfig() (Config, error) {
 	var cfg Config
 
@@ -28,10 +32,13 @@ func GetConfig() (Config, error) {
 	conf.Var(conf.Environ, &cfg.HelmLogin.Registry, "HELM_REGISTRY")
 	conf.Var(conf.Environ, &cfg.HelmLogin.CredentialsPath, "HELM_REGISTRY_CREDENTIALS_PATH")
 	conf.Var(conf.Environ, &cfg.ChartCacheDir, "CHART_CACHE_DIR", conf.RequiredNonEmpty[string]())
+	conf.Var(conf.Environ, &cfg.Concurrency, "CONCURRENCY", conf.Default(defaultConcurrency))
 
 	if err := conf.Environ.Parse(); err != nil {
 		return cfg, fmt.Errorf("failed to parse environment: %w", err)
 	}
+
+	cfg.Concurrency = max(cfg.Concurrency, 1)
 
 	if cfg.HelmLogin.Registry != "" {
 		var err error
