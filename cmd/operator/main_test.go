2bc2f2b0820d4d204c9814b3a90f1002d21288df
David Desmarais-Michaud <desmarais.michaud.david@gmail.com>
2026-07-17 12:06:17 -0400

    fix(PL-6888): fix destination server to https protocol

--- cmd/operator/main_test.go
+++ cmd/operator/main_test.go
@@ -381,7 +381,7 @@ func TestHappyReconciliations(t *testing.T) {
 				},
 				app.Spec.Source,
 			)
-			require.Equal(t, argocd.ApplicationDestination{Server: "http://kubernetes.svc.local"}, app.Spec.Destination)
+			require.Equal(t, argocd.ApplicationDestination{Server: "https://kubernetes.svc.local"}, app.Spec.Destination)
 		},
 		"staging": func(t *testing.T, app *argocd.Application) {
 			require.Equal(t, "argocd", app.Namespace)
@@ -402,7 +402,7 @@ func TestHappyReconciliations(t *testing.T) {
 			require.Equal(
 				t,
 				argocd.ApplicationDestination{
-					Server:    "http://kubernetes.svc.local",
+					Server:    "https://kubernetes.svc.local",
 					Namespace: "staging",
 				},
 				app.Spec.Destination,
